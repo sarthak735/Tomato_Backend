39c5020f39d5481be9e642222cc6003253474255
Kunwar Sarthak Singh <kuwarsarthaksingh735@gmail.com>
2024-05-16 10:17:33 +0530

    Created the userModel and controller for user login and resgister

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ import express from "express"
 import cors from "cors"
 import { connectDb } from "./config/db.js"
 import foodRouter from "./routes/foodRoute.js"
+import userRouter from "./routes/userRoute.js"
+import 'dotenv/config'
 
 
 
@@ -22,6 +24,7 @@ connectDb();
 // api endpoints
 app.use("/api/food", foodRouter)
 app.use('/images', express.static('uploads'))
+app.use("/api/user", userRouter)
 
 
 app.get('/', (req,res) => {
